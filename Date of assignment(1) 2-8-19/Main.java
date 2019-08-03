e53c07ef98a9101162678600b37be0d94c798a75
Twinkle Lahariya <twinkle.lahariya@synapse.com>
2019-08-03 19:04:27 +0530

    Sorting and removing duplicates

--- Date of assignment(1) 2-8-19/Main.java
+++ Date of assignment(1) 2-8-19/Main.java
@@ -1,17 +1,33 @@
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * 
+ * @author twinkle.lahariya :::program to sort given arraylist and to remove
+ *         duplicates from the array
+ */
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		System.out.print("Get set...\n");
-		
-		List<Integer> array = Arrays.asList(5,89,6,23,68);
-		
-		array.sort((s1,s2)->s1.compareTo(s2));
-		array.forEach(System.out::println);
+
+		List<Integer> arraySort = Arrays.asList(5, 89, 6, 23, 68);
+
+		List<Integer> arrayDistinct = Arrays.asList(5, 5, 6, 6, 2, 2, 2, 2, 3, 3, 6, 2, 2, 6, 8, 9, 5, 3, 2, 1);
+
+		System.out.println("SOrted list is::");
+		arraySort.sort((s1, s2) -> s1.compareTo(s2));
+		arraySort.forEach(System.out::println);
+
+		System.out.println("After removing duplicates the array is::");
+
+		Stream<Integer> arr = arrayDistinct.stream().distinct();
+
+		arr.forEach(System.out::println);
 	}
 
 }
