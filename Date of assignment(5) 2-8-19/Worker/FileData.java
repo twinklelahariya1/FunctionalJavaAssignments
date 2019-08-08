6a1a653191f76503925da8c4403de72b85d99b9c
twinklelahariya1 <52948286+twinklelahariya1@users.noreply.github.com>
2019-08-08 11:19:02 +0530

    Update FileData.java

--- Date of assignment(5) 2-8-19/Worker/FileData.java
+++ Date of assignment(5) 2-8-19/Worker/FileData.java
@@ -1,5 +1,5 @@
 package Worker;
-
+//convenstions a mustttttttttttt
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,22 +8,22 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class FileData {
-
+				   //parameterize
 	public static void listName() throws IOException {
 		Files.list(Paths.get("D:\\AssignmentDemo")).filter(Files::isRegularFile).forEach(System.out::println);
-
+					//never hardcode-->user to choose dir-->
 		displayFileData();
 	}
 
-	public static void extensionSpecificFile() throws IOException {
+	public static void extensionSpecificFile() throws IOException {			//overloading
 		File directory = new File("D:\\AssignmentDemo");
 		Scanner sr = new Scanner(System.in);
-
+			//ex: inputReader instead of sr
 		System.out.println("Enter extension choice\n1:.txt\n2:.jpg\n3:.java");
 		String extension = sr.nextLine();
 
 		File[] files = directory.listFiles((dir, name) -> name.toLowerCase().endsWith(extension));
-		for (File f : files)
+		for (File f : files)	//use {} even for single statements
 			System.out.println(f.getName());
 		sr.close();
 		displayFileData();
@@ -36,7 +36,7 @@ public class FileData {
 
 		System.out.println("Enter the name of file to display content");
 		String fileName = sr.nextLine();
-		Stream<String> stream = Files.lines(Paths.get(fileName));
+		Stream<String> stream = Files.lines(Paths.get(fileName));// large file go for buffer reader
 		stream.forEach(System.out::println);
 		
 		sr.close();
