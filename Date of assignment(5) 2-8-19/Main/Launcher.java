e6663356e79b68e62fe9c75cdb06da99642f4717
twinklelahariya1 <52948286+twinklelahariya1@users.noreply.github.com>
2019-08-08 11:19:11 +0530

    Update Launcher.java

--- Date of assignment(5) 2-8-19/Main/Launcher.java
+++ Date of assignment(5) 2-8-19/Main/Launcher.java
@@ -6,13 +6,13 @@ import Worker.FileData;
 
 public class Launcher {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {		//kuch nai chahiye---> invoke method
 
 		System.out.println(
 				"Operations::\n1: List names of files in directory.\n2: List files with specific extension");
-		Scanner sr = new Scanner(System.in);
-		int choice = sr.nextInt();
-		switch (choice) {
+		Scanner sr = new Scanner(System.in);//supplier<int>....... interface 
+		int choice = sr.nextInt();//enums
+		switch (choice) {         ///mp<enum,fn>  --> replaces switch case--->conditions are bad for coding practice
 		case 1:
 			FileData.listName();
 			break;
