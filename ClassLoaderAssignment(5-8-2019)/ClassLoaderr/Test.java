b1162b0353ad12310ea9a8f5361e839303cbea5b
twinklekailashlahariya <twinklekailashlahariya@Twinkles-MacBook-Pro.local>
2022-03-10 11:45:34 +0530

    Git config check

--- ClassLoaderAssignment(5-8-2019)/ClassLoaderr/Test.java
+++ ClassLoaderAssignment(5-8-2019)/ClassLoaderr/Test.java
@@ -3,7 +3,7 @@ package ClassLoaderr;
 public class Test {
 
 	public void show() {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!!");
 		 System.out.println("running thread name is:"+Thread.currentThread().getPriority());
 	}
 }
